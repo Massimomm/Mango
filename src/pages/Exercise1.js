c1073d7b60651b6ac7dfb0d0d9469a3e34cfdbc8
Massimomm <massimomm@hotmail.com>
2021-01-12 19:35:38 +0100

    Start Exercise1

--- src/pages/Exercise1.js
+++ src/pages/Exercise1.js
@@ -2,18 +2,30 @@ import React, { useState, useEffect, memo } from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 import Slider from "@material-ui/core/Slider";
+import Input from "@material-ui/core/Input";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"; // v1.x
 import { fade } from "@material-ui/core/styles/colorManipulator";
 
-// https://material-ui.com/components/slider/
 const Exercise1 = () => {
-  const [value, setValue] = useState([1, 1000]);
+  const [value, setValue] = useState([]);
+  const [maxValue, setMaxValue] = useState(0);
+  const [minValue, setMinValue] = useState(0);
   const [step] = useState(1);
 
   useEffect(() => {
+    fetch("https://demo8878015.mockable.io/mangoExercise1")
+      .then((res) => res.json())
+      .then((result) => {
+        const { range } = result;
+        setValue(range);
+        setMinValue(range[0]);
+        setMaxValue(range[1]);
+      });
+
     return () => {};
-  }, [value]);
+  }, []);
 
   const useStyles = makeStyles({
     typography: {
@@ -30,6 +42,10 @@ const Exercise1 = () => {
       width: 500,
       color: "red",
     },
+    input: {
+      marginLeft: 12,
+      width: 54,
+    },
   });
 
   const theme = createMuiTheme({
@@ -37,8 +53,8 @@ const Exercise1 = () => {
       MuiSlider: {
         track: { backgroundColor: "green", height: 6 },
         thumb: {
-          height: 18,
-          width: 18,
+          height: value[1] === value[0] ? 28 : 18,
+          width: value[1] === value[0] ? 28 : 18,
           backgroundColor: "#fff",
           border: "2px solid #de235b",
           "&$focused, &:hover": {
@@ -61,31 +77,70 @@ const Exercise1 = () => {
     },
   });
 
-  const marks = [
-    {
-      value: 1,
-      label: "1 €",
-    },
-    {
-      value: 1000,
-      label: "1000 €",
-    },
-  ];
-
   function valuetext(value) {
     return `${value} €`;
   }
+  const handleInputChange = (event) => {
+    const {
+      target: { value: val, id },
+    } = event;
+    console.log("handleInputChange", id, value, val);
+
+    let range = new Array();
+
+    switch (id) {
+      case "minValue":
+        const min = val ? (+val <= value[1] ? +val : undefined) : 1;
+        range[0] = min;
+        range[1] = maxValue ?? value[1];
+        setMinValue(+val);
+        // setMinValue(
+        //   +val > 0 && +val <= 1000
+        //     ? +val <= value[1]
+        //       ? +val
+        //       : value[0]
+        //     : undefined
+        // );
+        break;
+      case "maxValue":
+        const max = val ? (+val >= value[0] ? +val : undefined) : undefined;
+        range[0] = minValue ?? value[0];
+        range[1] = max;
+        setMaxValue(+val);
+        // setMaxValue(
+        //   +val > 0 && +val <= 1000 && +val >= value[0] ? +val : undefined
+        // );
+        break;
+      default:
+        break;
+    }
+
+    setValue(range);
+  };
+
+  const handleBlur = () => {
+    console.log("handleBlur", value);
+
+    // if (value < 0) {
+    //   setValue(0);
+    // } else if (value > 100) {
+    //   setValue(100);
+    // }
+  };
 
   const handleChange = (event, newValue) => {
     // console.log("handleChange", event, "min", newValue[0], "max", newValue[1]);
     // https://github.com/mui-org/material-ui/issues/17228
-    if (newValue && newValue.length) {
-      if (newValue[1] - newValue[0] <= 50) {
-        newValue[0] = newValue[0] - 50; // restrict the range to be 50
-        newValue[1] = newValue[1] + 50; // restrict the range to be 50
-        setValue(newValue);
-      } else setValue(newValue);
-    }
+    // if (newValue && newValue.length) {
+    //   if (newValue[1] - newValue[0] <= 50) {
+    //     newValue[0] = newValue[0] - 50; // restrict the range to be 50
+    //     newValue[1] = newValue[1] + 50; // restrict the range to be 50
+    //     setValue(newValue);
+    //   } else setValue(newValue);
+    // }
+    setMinValue(newValue[0]);
+    setMaxValue(newValue[1]);
+    setValue(newValue);
   };
 
   const classes = useStyles();
@@ -97,35 +152,70 @@ const Exercise1 = () => {
         variant="h4"
         gutterBottom
       >
-        Normal range
+        Normal Range
       </Typography>
-
-      <MuiThemeProvider theme={theme}>
-        <Slider
-          track={"normal"}
-          value={value}
-          onChange={handleChange}
-          valueLabelDisplay="on"
-          aria-labelledby="range-slider"
-          getAriaValueText={valuetext}
-          defaultValue={1}
-          marks={marks}
-          step={step}
-          min={1}
-          max={1000}
-        />
-      </MuiThemeProvider>
-
+      <Grid container spacing={2} alignItems="center">
+        <Grid item>
+          <Input
+            id={"minValue"}
+            className={classes.input}
+            value={minValue}
+            margin="dense"
+            onChange={(event) => handleInputChange(event)}
+            onBlur={handleBlur}
+            inputProps={{
+              step: { step },
+              min: 1,
+              max: 1000,
+              type: "number",
+              "aria-labelledby": "input-slider",
+            }}
+          />
+        </Grid>
+        <Grid item xs>
+          <MuiThemeProvider theme={theme}>
+            <Slider
+              track={"normal"}
+              value={value}
+              onChange={handleChange}
+              valueLabelDisplay="on"
+              aria-labelledby="range-slider"
+              getAriaValueText={valuetext}
+              defaultValue={1}
+              step={step}
+              min={1}
+              max={1000}
+            />
+          </MuiThemeProvider>
+        </Grid>
+        <Grid item>
+          <Input
+            id={"maxValue"}
+            className={classes.input}
+            value={maxValue}
+            margin="dense"
+            onChange={(event) => handleInputChange(event)}
+            onBlur={handleBlur}
+            inputProps={{
+              step: { step },
+              min: 1,
+              max: 1000,
+              type: "number",
+              "aria-labelledby": "input-slider",
+            }}
+          />
+        </Grid>
+      </Grid>
       {
         <Typography className={classes.typography}>
-          Selected Price Range {value[1] - value[0]} €
+          Range Price {value[1] - value[0]} €
         </Typography>
       }
 
-      {value[1] - value[0] < 70 && (
+      {maxValue < minValue && (
         <Typography className={classes.dangerText}>
-          The Price Range is low, Remember that Min value and Max value can't be
-          crossed {value[0]} € - {value[1]} €
+          The Price Range Selected is NOT allowed! Min and Max value can't be
+          crossed
         </Typography>
       )}
     </div>
--- src/pages/Exercise2.js
+++ src/pages/Exercise2.js
@@ -1,17 +1,141 @@
-import React from "react";
+// https://www.w3.org/TR/wai-aria-practices/examples/slider/multithumb-slider.html
+import React, { useState, useEffect, memo } from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"; // v1.x
+import { fade } from "@material-ui/core/styles/colorManipulator";
 
-const Exercise2 = () => {
-  console.log("Exercise 2");
-  // https://www.w3.org/TR/wai-aria-practices/examples/slider/multithumb-slider.html
+// https://material-ui.com/components/slider/
+const Exercise1 = () => {
+  const [value, setValue] = useState([1, 1000]);
+  const [step] = useState(1);
+
+  useEffect(() => {
+    return () => {};
+  }, [value]);
+
+  const useStyles = makeStyles({
+    typography: {
+      marginBottom: 50,
+      width: 500,
+      color: "#304ffe",
+    },
+    root: {
+      marginTop: 100,
+      marginLeft: 550,
+      width: 500,
+    },
+    dangerText: {
+      width: 500,
+      color: "red",
+    },
+  });
+
+  const theme = createMuiTheme({
+    overrides: {
+      MuiSlider: {
+        track: { backgroundColor: "green", height: 6 },
+        thumb: {
+          height: value[1] - value[0] > 50 ? 18 : 28,
+          width: value[1] - value[0] > 50 ? 18 : 28,
+          backgroundColor: "#fff",
+          border: "2px solid #de235b",
+          "&$focused, &:hover": {
+            boxShadow: `0px 0px 0px ${8}px ${fade("#de235b", 0.16)}`,
+          },
+          "&$activated": {
+            boxShadow: `0px 0px 0px ${8 * 1.5}px ${fade("#de235b", 0.16)}`,
+          },
+          "&$jumped": {
+            boxShadow: `0px 0px 0px ${8 * 4.5}px ${fade("#de235b", 0.16)}`,
+          },
+        },
+        trackAfter: {
+          backgroundColor: "#d0d7dc",
+        },
+        focused: {},
+        activated: {},
+        jumped: {},
+      },
+    },
+  });
+
+  const marks = [
+    {
+      value: 1,
+      label: "1 €",
+    },
+    {
+      value: 1000,
+      label: "1000 €",
+    },
+  ];
+
+  function valuetext(value) {
+    return `${value} €`;
+  }
+
+  const handleChange = (event, newValue) => {
+    // console.log("handleChange", event, "min", newValue[0], "max", newValue[1]);
+    // https://github.com/mui-org/material-ui/issues/17228
+    // if (newValue && newValue.length) {
+    //   if (newValue[1] - newValue[0] <= 50) {
+    //     newValue[0] = newValue[0] - 50; // restrict the range to be 50
+    //     newValue[1] = newValue[1] + 50; // restrict the range to be 50
+    //     setValue(newValue);
+    //   } else setValue(newValue);
+    // }
+    setValue(newValue);
+  };
+
+  const classes = useStyles();
   return (
-    <div>
-      <h3>Hotel Price Range</h3>
+    <div className={classes.root}>
+      <Typography
+        className={classes.typography}
+        id="range-slider"
+        variant="h4"
+        gutterBottom
+      >
+        Fixed Values Range
+      </Typography>
+
+      <MuiThemeProvider theme={theme}>
+        <Slider
+          track={"normal"}
+          value={value}
+          onChange={handleChange}
+          valueLabelDisplay="on"
+          aria-labelledby="range-slider"
+          getAriaValueText={valuetext}
+          defaultValue={1}
+          marks={marks}
+          step={step}
+          min={1}
+          max={1000}
+        />
+      </MuiThemeProvider>
+
+      {
+        <Typography className={classes.typography}>
+          Selected Price Range {value[1] - value[0]} €
+        </Typography>
+      }
+
+      {value[1] - value[0] < 70 && (
+        <Typography className={classes.dangerText}>
+          The Price Range is low, Remember that Min value and Max value can't be
+          crossed {value[0]} € - {value[1]} €
+        </Typography>
+      )}
     </div>
   );
 };
 
-Exercise2.defaultProps = {};
+Exercise1.defaultProps = {};
 
-Exercise2.propTypes = {};
+Exercise1.propTypes = {};
 
-export default Exercise2;
+export default memo(Exercise1);
